3e5fabcbcab5b58880556dedce5879e30aa1842c
Tomasz Derek <t.derek@g.loiv.torun.pl>
2018-06-29 01:05:29 +0200

    Update per.cpp

--- src/per.cpp
+++ src/per.cpp
@@ -11,15 +11,44 @@ public:
     double input_data_Y[3] = {0, 0, 1};
     double output_data[3] = {0, 0, 1};
     double weights[2];
+    double errors[2];
     double saved_weights[2];
+    double node_1, node_2;
     double counted_value;
     double error;
-   /* double stochastic_gradient_descent(double predict_value, int id)
+    bool czy_przerwac = false;
+    double fun(double value)
+    {
+        if(value<-1)
+        {
+            return -1;
+        }
+        else if(value > 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return value;
+        }
+    }
+    double mod(double value)
+    {
+        if(value < 0)
+        {
+            return -value;
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    double stochastic_gradient_descent(double predict_value, int id)
     {
         error = predict_value - output_data[id];
 
     }
-    */
     int learn()
     {
         srand(time(NULL));
@@ -29,29 +58,47 @@ public:
         }
         for(int i=0; i<1000; i++)
         {
+            cout << "Number of iteration: " << i+1 <<endl;
             cout << "Weights: " << weights[0] << " | " <<  weights[1] << endl;
             for(int i=0; i<3; i++)
             {
-                counted_value = input_data_X[i] * weights[0] + input_data_Y[i] * weights[1];
+                node_1 = fun(input_data_X[i] * weights[0]);
+                node_2 = fun(input_data_Y[i] * weights[1]);
+                counted_value = fun(node_1 + node_2);
                 cout << "Prediction for " << input_data_X[i] << " " << input_data_Y[i] << " is " << counted_value << endl;
-                error = output_data[i] - counted_value;
+                errors[0] = output_data[i] - 0.5*((counted_value - node_1)*(counted_value - node_1));
+                errors[1] = output_data[i] - 0.5*((counted_value - node_2)*(counted_value - node_1));
+                cout << errors[0] << " " << errors[1] << endl;
+                error = errors[0] + errors[1];
+                if(error > 1)
+                {
+                    errors[0] /= 2;
+                    errors[1] /= 2;
+                }
                 cout << "Error: " << error << endl;
-                double new_weight = output_data[i] - 0.5*(error*error);
-                cout << "New weight " << new_weight << endl;
                 if(error!=0)
                 {
-                    weights[0] = new_weight;
-                    weights[1] = new_weight;
+                    weights[0] = errors[0];
+                    weights[1] = errors[1];
+                }
+                if(error < 0.5 && i>10)
+                {
+                    czy_przerwac = true;
+                    break;
                 }
             }
+            saved_weights[0] = weights[0];
+            saved_weights[1] = weights[1];
+            if(czy_przerwac == true)
+            {
+                break;
+            }
         }
-        saved_weights[0] = weights[0];
-        saved_weights[1] = weights[1];
     }
     int predict(double x, double y)
     {
         cout << "Prediction for " << x << " " << y << " is ";
-        if(x * saved_weights[0] + y * saved_weights[1] > 0.5)
+        if(fun(x * saved_weights[0] + y * saved_weights[1]) > 0.5)
         {
             cout<<"1\n";
         }
