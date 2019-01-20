5fb50d13411b58f998c7d525578c5ce717910022
hyperscypion <tomasz_derek@wp.pl>
2019-01-20 14:00:06 +0100

    Some good stuff

--- src/per.cpp
+++ src/per.cpp
@@ -6,34 +6,64 @@
 
 using namespace std;
 
-class Perceptron
+class MultiLayerPerceptron
 {
 public:
     int input_size;
+    int hidden_size;
+    int output_size;
     double X_train;
     double y_train;
     vector <double> weights;
-    void set_weights(int input_size);
-    double dot_product(vector <double> weights, double X_train[]);
-    void train(vector <double> dot, double y_train[]);
-    void predict(vector <double> saved_weights, double X_test);
+    void set_weights(int input_size, int hidden_size, int output_size);
+    //double dot_product(vector <double> weights, double X_train[]);
+    //void train(vector <double> dot, double y_train[]);
+    //void predict(vector <double> saved_weights, double X_test);
 };
 
-void Perceptron::set_weights(int input_size)
+void MultiLayerPerceptron::set_weights(int input_size, int hidden_size, int output_size)
 {
     printf("Generowanie wag\n");
+    double first_matrix[input_size][hidden_size];
+    double second_matrix[hidden_size][output_size];
     for (int i = 0; i < input_size; ++i)
     {
-        weights.push_back(((double) rand() / (RAND_MAX)));
+	for (int j = 0; j < hidden_size; ++j)
+	{
+        	first_matrix[i][j] = ((double) rand() / (RAND_MAX));
+	}
     }
+    printf("Pierwsza macierz wag\n");
     for (int i = 0; i < input_size; ++i)
     {
-        cout << weights[i] << endl;
+	for (int j = 0; j < hidden_size; ++j)
+	{
+        	cout << first_matrix[i][j] << "   ";
+	}
+	cout << endl;
     }
+    // Druga macierz //
+    for (int i = 0; i < hidden_size; ++i)
+    {
+	for (int j = 0; j < output_size; ++j)
+	{
+		second_matrix[i][j] = ((double) rand() / (RAND_MAX));
+	}
+    }
+    printf("Druga macierze wag\n");
+    for (int i = 0; i < hidden_size; ++i)
+    {
+	for (int j = 0; j < output_size; ++j)
+	{
+		cout << second_matrix[i][j] << "   ";
+	}
+	cout << endl;
+    }
+
 
 }
-
-double Perceptron::dot_product(vector <double> weights, double input[])
+/*
+double MultiLayerPerceptron::dot_product(vector <double> weights, double input[])
 {
     double dot = 0;
     for (int i = 0; i < weights.size(); ++i)
@@ -53,7 +83,7 @@ double Perceptron::dot_product(vector <double> weights, double input[])
     }
 }
 
-void Perceptron::train(vector <double> X_train, double labels[])
+void MultiLayerPerceptron::train(vector <double> X_train, double labels[])
 {
     for (int i = 0; i < X_train.size(); ++i)
     {
@@ -62,21 +92,25 @@ void Perceptron::train(vector <double> X_train, double labels[])
 
 }
 
-void Perceptron::predict(vector <double> saved_weights, double X_test)
+void MultiLayerPerceptron::predict(vector <double> saved_weights, double X_test)
 {
 
 }
-
+*/
 int main()
 {
-    Perceptron per;
+    MultiLayerPerceptron per;
     printf("Podaj wielkosc wejscia\n");
     cin >> per.input_size;
-    per.set_weights(per.input_size);
+    printf("Podaj wielkosc warstwy ukrytej\n");
+    cin >> per.hidden_size;
+    printf("Podaj wielkosc wyjscia\n");
+    cin >> per.output_size;
+    per.set_weights(per.input_size, per.hidden_size, per.output_size);
     vector <double> data = {1, 2, 3};
     double data2[] = {1, 2, 3};
 
-    per.dot_product(per.weights, data2);
+    //per.dot_product(per.weights, data2);
     return 0;
 }
 
