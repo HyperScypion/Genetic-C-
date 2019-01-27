806728304dec7fa3f01ac403a56795766ae08a17
Tomasz Derek <tomasz_derek@wp.pl>
2019-01-27 23:05:17 +0100

    Update activation.h

--- src/activation.h
+++ src/activation.h
@@ -1,2 +1,13 @@
-#ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
+class Activation
+{
+public:
+    double relu(double x);
+    double softmax(double x);
+    double tanh(double x);
+    double relu_derative(double x);
+    double softmax_derative(double x);
+    double tanh_derative(double x);
+};
+
+
+
