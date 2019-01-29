626734cb1ab195161c64ea64b74812e9d39e8b0c
Tomasz Derek <tomasz_derek@wp.pl>
2019-01-29 02:18:08 +0100

    Update multilayerperceptron.cpp

--- src/multilayerperceptron.cpp
+++ src/multilayerperceptron.cpp
@@ -1,26 +1,82 @@
 #include <vector>
-#include <iostream>
+#include <random>
 #include <string>
+#include <iostream>
+#include "activation.h"
+#include "multilayerperceptron.h"
 
-class Network
+void Network::set_weights()
 {
-public:
-    int input_size;
-    int epochs;
-    double learning_rate;
-    void set_weights();
-    void train(double learning_rate, int epochs, std::vector <double> data, std::vector <double> labels, std::string activ);
-    double forward_prop(std::vector <double> data, std::string activ);
-private:
-    std::vector <double> weights;
+    for (int i = 0; i < input_size; ++i)
+    {
+        weights.push_back(((double) rand() / (RAND_MAX)));
+    }
 }
 
-class Perceptron:public class Network
+double Network::forward_prop(std::vector <double> data, std::string activ)
 {
-};
+    Activation activ_func;
+    double dot_product;
+    for (int i = 0; i < input_size; ++i)
+    {
+        dot_product += weights[i] * data[i];
+    }
+    if (activ == "relu")
+    {
+        return activ_func.relu(dot_product);
+    }
+    else if (activ == "softmax")
+    {
+        return activ_func.softmax(dot_product);
+    }
+    else if (activ == "tanh")
+    {
+        return activ_func.tanh(dot_product);
+    }
+}
 
-class Adaline:public class Network
+virtual void Network::train(double learning_rate, int epochs, std::vector <double> data, std::vector <double> labels, std::string activ)
 {
-public:
-    double get_gradient(std::string activ, double weight);
-};
+    double result;
+    for (int i = 0; i < epochs; ++i)
+    {
+        std::cout << "Epoch " << i+1 << "\\" << epochs << "\n";
+        result = forward_prop(data, activ);
+        for (int j = 0; j < data.size(); ++j)
+        {
+            std::cout << "Old weight = " << weights[j] << " indeks wagi =  " << j <<" \n";
+            weights[j] += learning_rate*(labels[0] - result)*data[j];
+            std::cout << "New weight = " << weights[j] << " indeks wagi = " << j << " \n";
+        }
+    }
+}
+
+double Adaline::get_gradient(std::string activ, double weight)
+{
+    Activation activ_func;
+    if (activ == "relu")
+    {
+        return activ_func.relu_derative(weight);
+    }
+    else if (activ == "softmax")
+        return activ_func.softmax_derative(weight);
+    else if (activ == "tanh")
+        return activ_func.tanh_derative(weight);
+}
+
+void Adaline::train(double learning_rate, int epochs, std::vector <double> data, std::vector <double> labels, std::string activ)
+{
+
+    double result;
+    for (int i = 0; i < epochs; ++i)
+    {
+        std::cout << "Epoch " << i+1 << "\\" << epochs << "\n";
+        result = forward_prop(data, activ);
+        for (int j = 0; j < data.size(); ++j)
+        {
+            std::cout << "Old weight = " << weights[j] << " indeks wagi =  " << j <<" \n";
+            weights[j] -= learning_rate*get_gradient(activ, weights[j]);
+            std::cout << "New weight = " << weights[j] << " indeks wagi = " << j << " \n";
+        }
+    }
+}
