ef820ff2edf51a184694ffe0c0d1c7dc2178fa6c
Tomasz Derek <tomasz_derek@wp.pl>
2019-01-29 02:17:55 +0100

    Update multilayerperceptron.h

--- src/multilayerperceptron.h
+++ src/multilayerperceptron.h
@@ -2,29 +2,26 @@
 #include <iostream>
 #include <string>
 
-class Perceptron
+class Network
 {
 public:
     int input_size;
     int epochs;
     double learning_rate;
     void set_weights();
+    virtual void train(double learning_rate, int epochs, std::vector <double> data, std::vector <double> labels, std::string activ);
     double forward_prop(std::vector <double> data, std::string activ);
-    void train(double learning_rate, int epochs, std::vector <double> data, std::vector <double> labels, std::string activ);
 private:
     std::vector <double> weights;
 };
 
-class Adaline
+class Perceptron:public Network
+{
+};
+
+class Adaline:public Network
 {
 public:
-    int input_size;
-    int epochs;
-    double learning_rate;
-    void set_weights();
-    double forward_prop(std::vector <double> data, std::string activ);
-    double get_gradient(std::string activ, double weight);
     void train(double learning_rate, int epochs, std::vector <double> data, std::vector <double> labels, std::string activ);
-private:
-    std::vector <double> weights;
+    double get_gradient(std::string activ, double weight);
 };
